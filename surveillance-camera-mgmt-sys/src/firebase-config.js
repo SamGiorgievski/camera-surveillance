2a15c9e43e55a583ad2b3b4c662a45217e1c7951
Samuel Giorgievski <SamGiorgievski@gmail.com>
2023-04-21 12:39:04 -0400

    firebase setup

---
+++ surveillance-camera-mgmt-sys/src/firebase-config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAiK2WOPPb4tcuvs2aj-pPXfBZjuPuvzfM",
+  authDomain: "camera-surveillance-18a9b.firebaseapp.com",
+  projectId: "camera-surveillance-18a9b",
+  storageBucket: "camera-surveillance-18a9b.appspot.com",
+  messagingSenderId: "879266863323",
+  appId: "1:879266863323:web:c5677ee1f6c291008f54fe",
+  measurementId: "G-PCP4624DQD"
+};
+
+const app = initializeApp(firebaseConfig);
