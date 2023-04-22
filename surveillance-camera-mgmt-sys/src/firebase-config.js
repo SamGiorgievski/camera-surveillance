609b5789b12c702d0e1ac628f064ef4b4b5b266f
Samuel Giorgievski <SamGiorgievski@gmail.com>
2023-04-22 15:48:41 -0400

    add storage to firebase config

--- surveillance-camera-mgmt-sys/src/firebase-config.js
+++ surveillance-camera-mgmt-sys/src/firebase-config.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAiK2WOPPb4tcuvs2aj-pPXfBZjuPuvzfM",
@@ -7,7 +8,11 @@ const firebaseConfig = {
   storageBucket: "camera-surveillance-18a9b.appspot.com",
   messagingSenderId: "879266863323",
   appId: "1:879266863323:web:c5677ee1f6c291008f54fe",
-  measurementId: "G-PCP4624DQD"
+  measurementId: "G-PCP4624DQD",
+  storageBucket: "camera-surveillance-18a9b.appspot.com"
 };
 
 const app = initializeApp(firebaseConfig);
+
+// Initialize Cloud Storage and get a reference to the service
+const storage = getStorage(app);
