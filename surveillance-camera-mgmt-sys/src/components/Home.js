540dd3f550dcf032180b816fc9461dba56d1444c
Samuel Giorgievski <SamGiorgievski@gmail.com>
2023-04-22 16:53:44 -0400

    add access to firebase videos

--- surveillance-camera-mgmt-sys/src/components/Home.js
+++ surveillance-camera-mgmt-sys/src/components/Home.js
@@ -1,9 +1,23 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import './Home.css';
+import { ref, getDownloadURL, listAll } from "firebase/storage";
+import { storage} from '../firebase-config';
+
+
 
 export default function Home() {
 
+  // Firebase
+  const storageRef = ref(storage);
+  const videosRef = ref(storage, 'cam-footage/');
+  const testRef = ref(storage, 'cam-footage/VID_20230422_151149.mp4');
+
+  // video state
+  const [videoUrl, setVideoUrl] = useState('');
+
+  
+  // logout function
     const handleLogout = () => {
         sessionStorage.removeItem('Auth Token');
         navigate('/login')
@@ -23,6 +37,21 @@ export default function Home() {
         }
     }, [])
 
+    useEffect(() => {
+
+      listAll(videosRef);
+
+      // Get the download URL of the video
+      getDownloadURL(testRef).then((url) => {
+        // Set the video URL in state
+        console.log(url);
+        setVideoUrl(url);
+      }).catch((error) => {
+        // Handle any errors
+        console.error(error);
+      });
+    }, []);
+
     return (
         <div className="home">
           <h1> Home Page </h1>
@@ -31,6 +60,9 @@ export default function Home() {
           <button onClick={handleLogout}>Log out</button>
           <div className="video">
             <p> Video </p>
+            {videoUrl && (
+              <video src={videoUrl} controls autoPlay />
+            )}
           </div>
         </div>
     )
--- surveillance-camera-mgmt-sys/src/firebase-config.js
+++ surveillance-camera-mgmt-sys/src/firebase-config.js
@@ -1,11 +1,10 @@
 import { initializeApp } from "firebase/app";
-import { getStorage } from "firebase/storage";
+import { getStorage} from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAiK2WOPPb4tcuvs2aj-pPXfBZjuPuvzfM",
   authDomain: "camera-surveillance-18a9b.firebaseapp.com",
   projectId: "camera-surveillance-18a9b",
-  storageBucket: "camera-surveillance-18a9b.appspot.com",
   messagingSenderId: "879266863323",
   appId: "1:879266863323:web:c5677ee1f6c291008f54fe",
   measurementId: "G-PCP4624DQD",
@@ -15,4 +14,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 // Initialize Cloud Storage and get a reference to the service
-const storage = getStorage(app);
+export const storage = getStorage(app);
+
+
